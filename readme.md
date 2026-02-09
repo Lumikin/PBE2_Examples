a45c1371fe829ccba21940ee80b44d43f6a7be24
lumikin <lumikomio02@gmail.com>
2026-02-09 09:26:29 -0300

    Inicializando o repositorio

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Node
+
+## package.json
+
+"type": "module"
+
+## Pacotes instalados
+
+    express mysql2 dotenv prisma@6 @prisma/client@6
+
+## Comandos adicionais
+- npx prima init (inicializar o prisma)
+- 
---
+++ src/controllers/produto.controller.js
@@ -0,0 +1,26 @@
+import prisma from "../models/prisma.client.js";
+
+const produtoController = {
+  criar: async (req, res) => {
+    try {
+      const { nome, preco } = req.body; //Implementar validações dos valores (campos) recebidos
+      const result = await prisma.produtos.create({
+        data: { nome, preco },
+      });
+      
+      // Create é como um insert sem precisar escrever todo o bloco
+
+      res.status(201).json({
+        message: " Registro Incluido com sucesso",
+      });
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Ocorreu um erro no servidor",
+      });
+    }
+  },
+};
+
+export default produtoController;
