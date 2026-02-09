b75a6d299a039dba7bbe4a2a5eeb2cc386c8f795
Lumikin <lumikomio02@gmail.com>
2026-02-09 12:52:27 -0300

    ref

--- src/controllers/produto.controller.js
+++ src/controllers/produto.controller.js
@@ -8,9 +8,7 @@ const produtoController = {
         data: { nomeProduto: nome, precoProduto: preco },
       }); // Create é como um insert sem precisar escrever todo o bloco
 
-      res.status(201).json({
-        message: "Registro Incluido com sucesso",
-      });
+      res.status(201).json({ message: "Registro Incluido com sucesso" });
     } catch (error) {
       console.error(error);
       res.status(500).json({
--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.json());
 app.use("/", produtoRoutes);
 
-
 app.listen(process.env.SERVER_PORT, () => {
   console.log(
     `Servidor rodando em http://localhost:${process.env.SERVER_PORT}`,
