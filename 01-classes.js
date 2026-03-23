91f00bf4d2119982f877bed4403a70c975fa1951
Lumikin <lumikomio02@gmail.com>
2026-03-23 13:19:25 -0300

    FEAT: Adiciona propriedade valor à classe Carro e atualiza método mostrarInfo

--- 01-classes.js
+++ 01-classes.js
@@ -1,24 +1,38 @@
 class Carro {
-// Propriedades privadas sao definidas com uso de #
+  // Propriedades privadas sao definidas com uso de #
   #marca;
   #modelo;
   #ano;
   #cor;
-// Construtor da classe que recebe os parametros para a criação do objeto
+  #valor;
+
+  // Construtor da classe que recebe os parametros para a criação do objeto
   constructor(marca, modelo, ano, cor) {
     this.#marca = marca;
     this.#modelo = modelo;
     this.#ano = ano;
     this.#cor = cor;
   }
-// Metodo para retornar a informação do objeto
+  // Metodo acessor para recuperar um valor
+
+  get valor() {
+    return this.#valor;
+  }
+  // metodo para retornar informação do objetivo
+  set valor(value) {
+    this.#valor = value;
+  }
+
+  // Metodo para retornar a informação do objeto
   mostrarInfo() {
-    return `Marca: ${this.#marca} | Modelo: ${this.#modelo} | Ano: ${this.#ano} | Cor: ${this.#cor}`;
+    return `Marca: ${this.#marca} | Modelo: ${this.#modelo} | Ano: ${this.#ano} | Cor: ${this.#cor} | Valor: ${this.#valor}`;
   }
 }
 
 const carro1 = new Carro("Chevrolet", "Cruze", 2025, "Cinza");
 const carro2 = new Carro("Volkswagen", "UP", 2022, "Branco");
+
+carro1.valor = 120000.0;
 
 console.log(carro1.mostrarInfo());
 console.log(carro2.mostrarInfo());
