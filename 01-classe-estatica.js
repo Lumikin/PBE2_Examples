cb34d1048bce59adfbee963655143efcb0b1dd5b
Lumikin <lumikomio02@gmail.com>
2026-03-23 13:35:22 -0300

    Feat: Adicionando o objeto de multiplicar

--- 01-classe-estatica.js
+++ 01-classe-estatica.js
@@ -1,7 +1,13 @@
 class Calculadora {
-  static somar(num1, num2) {
+  
+static somar(num1, num2) {
     return num1 + num2;
+  }
+
+static multiplicar(a, b, c) {
+    return a * b * c;
   }
 }
 
 console.log(Calculadora.somar(4, 10));
+console.log(Calculadora.multiplicar(2, 3, 4));
